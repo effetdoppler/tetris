52f0574727be59ff577814f735f6203848ce7d2e
Charles Zhang <charles.zhang@epita.fr>
2022-01-01 16:44:22 +0100

    create grid

--- Tetris.py
+++ Tetris.py
@@ -7,9 +7,21 @@ class Application(Frame):
         self.pack()
         self.width = x
         self.height = y
-        self.can = Canvas(self, width=self.width, height=self.height, bg='ivory', bd=3, relief=SUNKEN)
+        self.can = Canvas(self, width=self.width, height=self.height, bg='black', relief=SUNKEN)
         self.can.pack()
+        self.grid()
+
+    def grid(self):
+        gap = 0
+        for i in range(10):
+            gap += 20
+            self.can.create_line(gap, 0, gap, self.height, fill="green")
+        gap = 0
+        for i in range(20):
+            gap += 20
+            self.can.create_line(0, gap, self.width, gap, fill="green")
+
 
 
 if __name__ == '__main__':
-    Application(300, 400).mainloop()
+    Application(200, 400).mainloop()
