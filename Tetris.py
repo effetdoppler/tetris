4f9a96d0029a21c53b204a9ad8757cfc344f5e83
Charles Zhang <charles.zhang@epita.fr>
2022-01-01 17:18:31 +0100

    class block

--- Tetris.py
+++ Tetris.py
@@ -10,6 +10,7 @@ class Application(Frame):
         self.can = Canvas(self, width=self.width, height=self.height, bg='black', relief=SUNKEN)
         self.can.pack()
         self.grid()
+        self.block = Block(self, self.can)
 
     def grid(self):
         gap = 0
@@ -20,6 +21,17 @@ class Application(Frame):
         for i in range(20):
             gap += 20
             self.can.create_line(0, gap, self.width, gap, fill="green")
+
+
+class Block(object):
+    def __init__(self, app, can):
+        self.can = can
+        self.app = app
+        self.size = 20
+        self.spawn()
+
+    def spawn(self):
+        self.body = self.can.create_rectangle(self.app.width/2, 0, self.app.width/2+20, 20, fill="red")
 
 
 
